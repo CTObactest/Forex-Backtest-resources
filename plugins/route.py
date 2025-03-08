182a8f28788db3d68bd5decf4703d4edf4ade5f1
CTO <139733286+CTObactest@users.noreply.github.com>
2025-03-08 08:28:58 +0300

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -11,4 +11,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("CodeXBotz")
+    return web.json_response("The bot has restarted successfully. Return to the channel to find the series you want to watch")
