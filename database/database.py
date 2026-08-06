6daaa2bda1c961b27b204c6d7debfb7e7d6e557f
CTO <139733286+CTObactest@users.noreply.github.com>
2026-08-06 22:00:08 +0300

    Switch to async MongoDB client and update queries

--- database/database.py
+++ database/database.py
@@ -1,13 +1,10 @@
 #(©)CodeXBotz
 
-
-
-
-import pymongo, os
+import motor.motor_asyncio
 from config import DB_URI, DB_NAME
 
 
-dbclient = pymongo.MongoClient(DB_URI)
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
 
 
@@ -16,21 +13,21 @@ user_data = database['users']
 
 
 async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
+    found = await user_data.find_one({'_id': user_id})
     return bool(found)
 
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
+    await user_data.insert_one({'_id': user_id})
     return
 
 async def full_userbase():
     user_docs = user_data.find()
     user_ids = []
-    for doc in user_docs:
+    async for doc in user_docs:
         user_ids.append(doc['_id'])
-        
+
     return user_ids
 
 async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
+    await user_data.delete_one({'_id': user_id})
     return
