2e6a92353785f780454be06679a475c035be5a64
CTO <139733286+CTObactest@users.noreply.github.com>
2026-08-25 17:32:39 +0300

    Simplify start and end argument handling
    Refactor argument parsing to remove division by channel ID.

--- plugins/start.py
+++ plugins/start.py
@@ -70,8 +70,8 @@ async def start_command(client: Client, message: Message):
 
         if len(argument) == 3:
             try:
-                start = int(int(argument[1]) / abs(client.db_channel.id))
-                end = int(int(argument[2]) / abs(client.db_channel.id))
+                start = int(argument[1])
+                end = int(argument[2])
             except:
                 return
             if start <= end:
@@ -86,7 +86,7 @@ async def start_command(client: Client, message: Message):
                         break
         elif len(argument) == 2:
             try:
-                ids = [int(int(argument[1]) / abs(client.db_channel.id))]
+                ids = [int(argument[1])]
             except:
                 return
         temp_msg = await message.reply("Please wait...")
